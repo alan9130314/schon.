39db2ad03e8efcf5d03dbe1d5ae2d44415940806
Alan Wang <alan9130314@gmail.com>
2019-12-04 14:32:55 +0800

    20191204 Added productlist page

--- offical sites/js/action.js
+++ offical sites/js/action.js
@@ -95,11 +95,22 @@ $(document).ready(function () {
       // You can unslick at a given breakpoint now by adding:
       // settings: "unslick"
       // instead of a settings object
-    ]
-  
-
+    ]  
+  });
+  $("#example_id").ionRangeSlider();
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 10,
+    to: 599,
+    skin: "round",
+    onChange: function (data) {
+      $('span.PFfrom').text(('$')+data.from);
+      $('span.PFto').text(('$')+data.to);  
+  }  
   });
 
-
+ 
 
 });
